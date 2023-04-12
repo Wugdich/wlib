baa5ed0ee989be71eda9f7a60e969bcb2147c828
Wugd <sintes4ek@gmail.com>
2023-04-12 17:45:55 +0300

    Add book library page logic

--- wlib/libs/views.py
+++ wlib/libs/views.py
@@ -6,3 +6,8 @@ def home(requests):
     context = {
             }
     return render(requests, 'libs/home.html', context)
+
+def book_lib(requests):
+    context = {
+            }
+    return render(requests, 'libs/book_lib.html', context)
